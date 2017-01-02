029b9e09ab2212ee3633608b069b75eb9314dec2
steffencm <steffencm@gmail.com>
2017-01-02 16:15:55 -0500

    Add drop to first list

--- src/first.rs
+++ src/first.rs
@@ -43,6 +43,18 @@ impl List{
 
 }
 
+impl Drop for List{
+    fn drop(&mut self){
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        // while let == "do this until the pattern doesn't match"
+        while let Link::More(mut boxed_node) = cur_link{
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+            //Boxed node goes out of scope and gets dropped here
+            //but its Node's next field has been set to Link:: Empty
+            //so no unbounded recursion happens here
+        };
+    }
+}
 
 mod test {
     #[test]
