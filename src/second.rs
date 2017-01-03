5003c6117c9fb71ca07169f99c377658f29bda98
steffencm <steffencm@gmail.com>
2017-01-02 20:07:19 -0500

    Add a into iter method to second list

--- src/second.rs
+++ src/second.rs
@@ -59,6 +59,21 @@ impl<T> Drop for List<T>{
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T>{
+    fn into_iter(self) -> IntoIter<T>{
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T>{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item>{
+        self.0.pop()
+    }
+}
+
 mod test {
     use super::List;
 
@@ -104,5 +119,17 @@ mod test {
         //Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
     }
 }
